b637d5dc609b0869768e3eaafd0f48e7fd50f4b3
Brent Mitchell <brent.mitchell25@gmail.com>
2014-09-24 20:31:18 -0500

    Adding Prototype I

---
+++ Prototype 1/Assets/Scripts/FirstPersonController.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FirstPersonController : MonoBehaviour {
+	public float movementSpeed = 10.0f;
+	public float mouseSensitivity = 2.0f;
+	float verticalRoation = 0;
+	public float upDownRange = 60.0f;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float rotLeftRight = Input.GetAxis ("Mouse X") * mouseSensitivity;
+		transform.Rotate (0, rotLeftRight, 0);
+
+		verticalRoation = verticalRoation - Input.GetAxis ("Mouse Y") * mouseSensitivity;
+		verticalRoation = Mathf.Clamp (verticalRoation, -upDownRange, upDownRange);
+		Camera.main.transform.localRotation = Quaternion.Euler (verticalRoation, 0, 0);
+
+		float forwardSpeed = Input.GetAxis ("Vertical") * movementSpeed;
+		float sideSpeed = Input.GetAxis ("Horizontal") * movementSpeed;
+
+		Vector3 speed = new Vector3 (sideSpeed,0,forwardSpeed);
+		speed = transform.rotation * speed;
+		CharacterController cc = GetComponent<CharacterController>();
+		cc.SimpleMove (speed);
+	}
+}
