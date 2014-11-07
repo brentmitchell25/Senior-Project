ae01d155d869c178c2d8efd2e1acb3160c9574a7
brentmitchell25 <brent.mitchell25@gmail.com>
2014-11-07 13:21:43 -0600

    Adding OVR assets folder for movement and tracking, taken from Rift sdk

---
+++ Game/Assets/Scripts/Player Movement/PlayerMovement.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMovement : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey ("w")) {
+
+		}
+		if (Input.GetKeyDown ("s")) {
+
+		}
+		if (Input.GetKeyUp ("a")) {
+
+		}
+		if (Input.GetMouseButton (1)) {
+			// right mouse button
+			// 0 for left
+		}
+	}
+}
