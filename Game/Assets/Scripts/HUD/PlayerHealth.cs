bdcab7f52f8767a4c85869713fdb10ad5e549d86
TylerYoder <tyleryoderku@gmail.com>
2015-01-13 09:20:34 -0600

    Started PlayerHealth
    PlayerHealth script added to move HealthSlider when player takes damage.
    Mana, Stamina, and Experience Sliders should be very similar.

---
+++ Game/Assets/Scripts/HUD/PlayerHealth.cs
@@ -0,0 +1,67 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class PlayerHealth : MonoBehaviour {
+    public int maxHealth = 100;
+    public int curHealth;
+    //public Image damageImage;
+    public Slider healthSlider;
+    public float flashSpeed = 5f;
+    public Color flashColor = new Color(1f, 0f, 0f, .1f);
+
+    //Animator anim;
+    //AudioSource playerAudio;
+    CharacterMotor CharacterMotor;
+    bool isDead;
+    bool damaged;
+
+	// Use this for initialization
+	void Start () {
+        //anim = GetComponent<Animator>();
+        //playerAudio = GetComponent<AudioSource>();
+        CharacterMotor = GetComponent<CharacterMotor>();
+        curHealth = maxHealth;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (damaged)
+        {
+            //damageImage.color = flashColor;
+        }
+        else
+        {
+            //damageImage.color = Color.Lerp(damageImage.color, Color.clear, flashSpeed * Time.deltaTime);
+        }
+        damaged = false;
+	}
+
+    public void TakeDamage(int amount)
+    {
+        damaged = true;
+        curHealth -= amount;
+        healthSlider.value = (curHealth / maxHealth) * healthSlider.maxValue;
+        //playerAudio.Play();
+        if (curHealth <= 0 && !isDead)
+        {
+            Death();
+        }
+
+    }
+
+    void Death()
+    {
+        isDead = true;
+        //anim.SetTrigger("Die");
+        //playerAudio.clip = deathClip;
+        //playerAudio.Play();
+        CharacterMotor.canControl = false;
+
+    }
+
+    void onGUI()
+    {
+        
+    }
+}
