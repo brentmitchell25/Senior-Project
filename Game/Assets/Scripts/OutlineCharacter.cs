1b6a1934e8bcbe5c06a6a5ac3657942e215e44c7
brentmitchell25 <brent.mitchell25@gmail.com>
2015-02-05 14:56:08 -0600

    Adding visibility check to characters

---
+++ Game/Assets/Scripts/OutlineCharacter.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class OutlineCharacter : MonoBehaviour {
+	private GameObject player;
+
+	void Start() {
+		Debug.Log ("Start");
+		}
+
+	void Update()
+	{
+		if (gameObject.name.Equals("ArcherBowHighlighted")) {
+			Debug.Log (gameObject.ToString ());
+						if (renderer.IsVisibleFrom (GameObject.Find ("RightEyeAnchor").camera)) {
+								Debug.Log ("Visible");
+								gameObject.SetActive (true);
+						} else {
+								Debug.Log ("Not visible");
+								gameObject.SetActive (false);
+						}
+				}
+
+	}
+}
