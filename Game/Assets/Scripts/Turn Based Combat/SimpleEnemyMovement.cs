c27f1e13e73977dd26cd59502cc941fb1eabf6c6
Brent Mitchell <brent.mitchell25@gmail.com>
2015-02-05 18:35:21 -0600

    Finally reverting back to enemy AI movement

---
+++ Game/Assets/Scripts/Turn Based Combat/SimpleEnemyMovement.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SimpleEnemyMovement : MonoBehaviour {
+
+    Transform player;
+    GUIControls GUIcontrols;
+    SimpleEnemyHealth enemyHealth;
+    NavMeshAgent nav;
+
+    void Awake()
+    {
+        player = GameObject.FindGameObjectWithTag ("Player").transform;
+        GUIcontrols = player.GetComponent<GUIControls>();
+        enemyHealth = GetComponent<SimpleEnemyHealth>();
+        nav = GetComponent<NavMeshAgent>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        // If the enemy and the player have health left...
+        if (enemyHealth.curHealth > 0 && GUIcontrols.curHealth > 0)
+        {
+            // ... set the destination of the nav mesh agent to the player.
+            nav.SetDestination(player.position);
+        }
+        // Otherwise...
+        else
+        {
+            // ... disable the nav mesh agent.
+            nav.enabled = false;
+        }
+	}
+}
