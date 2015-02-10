8eafa88ecbdaa146d98e86448675088681f08545
Brent Mitchell <brent.mitchell25@gmail.com>
2015-02-09 19:11:37 -0600

    Now calculates the dot product with the archer. Should be pretty easy to implement on other characters, but need to worry about global variables.

--- Game/Assets/Scripts/OutlineCharacter.cs
+++ Game/Assets/Scripts/OutlineCharacter.cs
@@ -1,28 +1,37 @@
-﻿using UnityEngine;
-using System.Collections;
+﻿using System.Collections;
+using UnityEngine;
 
 public class OutlineCharacter : MonoBehaviour
 {
 		private static Shader shaderNoOutline = Shader.Find ("Diffuse");
 		private static Shader shaderOutline = Shader.Find ("Outlined/Diffuse");
-
-		private void RenderVisibility ()
-		{
-
-		}
+	private Camera camera;
+	
 
 		void Start ()
 		{
 				Debug.Log ("Start");
+			foreach (Camera c in Camera.allCameras) {
+						if (c.gameObject.name.Equals ("RightEyeAnchor")) {
+								camera = c.gameObject.camera;
+								break;
+						}
+				}
 		}
 
 		void Update ()
 		{
-				if (renderer.IsVisibleFrom (GameObject.Find ("RightEyeAnchor").camera)) {
+
+		Vector3 cam = Vector3.Normalize (camera.transform.forward);
+		//Debug.Log (camera.);
+		Vector3 archer = Vector3.Normalize(this.gameObject.transform.position);
+		float dotProduct = Vector3.Dot (cam, archer);
+				if (renderer.IsVisibleFrom (camera) && dotProduct > 0 && dotProduct < 90){
 						renderer.material.shader = shaderOutline;
 				} else {
 						renderer.material.shader = shaderNoOutline;
 				}
+		Debug.Log (Vector3.Dot (cam, archer));
 
 		}
 }
--- Game/Assets/Scripts/RendererExtensions.cs
+++ Game/Assets/Scripts/RendererExtensions.cs
@@ -5,7 +5,6 @@ public static class RendererExtensions
 	public static bool IsVisibleFrom(this Renderer renderer, Camera camera)
 	{
 		if (camera != null) {
-			Debug.Log (camera.camera);
 						Plane[] planes = GeometryUtility.CalculateFrustumPlanes (camera);
 						return GeometryUtility.TestPlanesAABB (planes, renderer.bounds);
 		}
