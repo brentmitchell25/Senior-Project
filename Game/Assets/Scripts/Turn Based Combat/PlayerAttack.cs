64efb29ec62b06b71a3de7942081b977883d7b7c
TylerYoder <tyleryoderku@gmail.com>
2015-02-09 20:39:46 -0600

    Added PlayerAttack
    Haven't finished, but player should be able to hit enemies and items
    with Shootable tag soon.

---
+++ Game/Assets/Scripts/Turn Based Combat/PlayerAttack.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerAttack : MonoBehaviour {
+    public float timeBetweenAttacks = .5f;
+    public int attackDamage = 20;
+    public float range = 5f;
+
+    float timer;
+    int shootableMask;
+
+	// Use this for initialization
+	void Awake () {
+        shootableMask = LayerMask.GetMask("Shootable");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    //Add time since last update was called to timer
+        timer += Time.deltaTime;
+
+        //If the attack button is being pressed and you can fire, do so
+        if (Input.GetButton("Fire1") && timer >= timeBetweenAttacks)
+        {
+            Attack();
+        }
+    }
+        void Attack()
+        {
+            //Reset the timer
+            timer = 0f;
+
+            ////TODO: Finish this
+
+        }
+
+}
