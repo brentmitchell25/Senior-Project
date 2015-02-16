5ce4e07e5d45cf2fef2ad9b503ce9194d34461c3
brentmitchell25 <brent.mitchell25@gmail.com>
2015-02-15 19:10:15 -0600

    Added text to the screen. Script now also assigns the character which is selected.

--- Game/Assets/Scripts/OutlineCharacter.cs
+++ Game/Assets/Scripts/OutlineCharacter.cs
@@ -6,41 +6,50 @@ public class OutlineCharacter : MonoBehaviour
 		private static Shader shaderNoOutline = Shader.Find ("Diffuse");
 		private static Shader shaderOutline = Shader.Find ("Outlined/Diffuse");
 		private Camera camera;
-	public float angle;
-	
+		public float angle;
 
 		void Start ()
 		{
 				Debug.Log ("Start");
-			foreach (Camera c in Camera.allCameras) {
+				foreach (Camera c in Camera.allCameras) {
 						if (c.gameObject.name.Equals ("RightEyeAnchor")) {
 								camera = c.gameObject.camera;
 								break;
 						}
 				}
-		gameObject.CompareTag ("Archer");
-		//GUI.Label(Rect(0,0,Screen.width,Screen.height),"Here is a block of text\nlalalala\nanother line\nI could do this all day!");
 		}
 
 		void Update ()
 		{
 
-		Vector3 cam = Vector3.Normalize (camera.transform.forward);
-		Vector3 archer = Vector3.Normalize(this.gameObject.transform.position);
-		angle = Vector3.Dot (cam, archer);
-		if (gameObject.CompareTag ("Archer"))
+				Vector3 cam = Vector3.Normalize (camera.transform.forward);
+				Vector3 archer = Vector3.Normalize (this.gameObject.transform.position);
+				// Assign angles
+				angle = Vector3.Dot (cam, archer);
+				if (gameObject.CompareTag ("Archer"))
 						GameInformation.ArcherAngle = angle;
-		else if (gameObject.CompareTag("Melee"))
-			GameInformation.MeleeAngle = angle;
-		else
-			GameInformation.MageAngle = angle;
-		
-		if (renderer.IsVisibleFrom (camera) && angle == Mathf.Max(new float[] { GameInformation.ArcherAngle,GameInformation.MeleeAngle,GameInformation.MageAngle })){
+				else if (gameObject.CompareTag ("Melee"))
+						GameInformation.MeleeAngle = angle;
+				else
+						GameInformation.MageAngle = angle;
+
+				// Select character with highest dot product to camera normal
+				if (renderer.IsVisibleFrom (camera) && angle == Mathf.Max (new float[] {
+						GameInformation.ArcherAngle,
+						GameInformation.MeleeAngle,
+						GameInformation.MageAngle
+				})) {
 						renderer.material.shader = shaderOutline;
+
+						if (gameObject.CompareTag ("Archer"))
+								GameInformation.PlayerClass = new BaseRangedClass ();
+						else if (gameObject.CompareTag ("Melee"))
+								GameInformation.PlayerClass = new BaseWarriorClass ();
+						else
+								GameInformation.PlayerClass = new BaseMageClass ();
 				} else {
 						renderer.material.shader = shaderNoOutline;
 				}
-		Debug.Log (angle);
 
 		}
 }
