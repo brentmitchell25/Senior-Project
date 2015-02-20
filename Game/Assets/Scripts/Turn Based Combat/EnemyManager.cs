d911a8b293dc4da41fb37c683c2fec0a7d06b528
TylerYoder <tyleryoderku@gmail.com>
2015-02-19 19:15:19 -0600

    Created EnemyManager (Spawns enemies)
    Committing before potentially screwing up enemy prefab.

---
+++ Game/Assets/Scripts/Turn Based Combat/EnemyManager.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyManager : MonoBehaviour {
+    public GUIControls GUIcontrols;
+    public GameObject enemy;
+    public float spawnTime = 3f;
+    public Transform[] spawnPoints;
+	// Use this for initialization
+	void Start () {
+        InvokeRepeating("Spawn", spawnTime, spawnTime);
+	}
+	
+	// Update is called once per frame
+	void Spawn () {
+	    //if player has no health left, don't spawn any more
+        if (GUIcontrols.curHealth <= 0f)
+        {
+            return;
+        }
+
+        //Find a random index between zero and one less than the number of spawn points
+        int spawnPointIndex = Random.Range(0, spawnPoints.Length);
+
+        //Create an instance of enemy prefab at the random spawn point
+        Instantiate(enemy, spawnPoints[spawnPointIndex].position, spawnPoints[spawnPointIndex].rotation);
+    }
+}
