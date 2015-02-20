b21c03013b3ea4ed86979a07bdc8aeaaaf14b1a3
TylerYoder <tyleryoderku@gmail.com>
2015-02-19 19:37:13 -0600

    EnemyManager Completed (Basic)
    Enemy prefab created.
    EnemyManager updated to instantiate enemy prefab.
    EnemyManager now has three test spawn points. Chooses between the three
    randomly and spawns there if curEnemyCount < maxEnemyCount.

--- Game/Assets/Scripts/Turn Based Combat/EnemyManager.cs
+++ Game/Assets/Scripts/Turn Based Combat/EnemyManager.cs
@@ -6,23 +6,29 @@ public class EnemyManager : MonoBehaviour {
     public GameObject enemy;
     public float spawnTime = 3f;
     public Transform[] spawnPoints;
+    public int curEnemyCount;
+    public int maxEnemyCount;
 	// Use this for initialization
 	void Start () {
+        curEnemyCount = 0;
+        maxEnemyCount = 2;
         InvokeRepeating("Spawn", spawnTime, spawnTime);
 	}
 	
 	// Update is called once per frame
 	void Spawn () {
-	    //if player has no health left, don't spawn any more
-        if (GUIcontrols.curHealth <= 0f)
+	    //if player has no health left, or if there is already the max number of enemies out, don't spawn any more
+        if (GUIcontrols.curHealth <= 0f || curEnemyCount >= maxEnemyCount)
         {
+            print("did not spawn, cur = " + curEnemyCount + ", max = " + maxEnemyCount);
             return;
         }
 
         //Find a random index between zero and one less than the number of spawn points
         int spawnPointIndex = Random.Range(0, spawnPoints.Length);
-
+        curEnemyCount += 1; 
         //Create an instance of enemy prefab at the random spawn point
         Instantiate(enemy, spawnPoints[spawnPointIndex].position, spawnPoints[spawnPointIndex].rotation);
+        print("spawned enemy at spawn point " + spawnPointIndex);
     }
 }
