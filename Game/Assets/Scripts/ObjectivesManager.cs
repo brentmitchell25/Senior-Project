ebad1cc7c49cc7f7e34b4ea53da7e0f74de80516
TylerYoder <tyleryoderku@gmail.com>
2015-03-09 18:35:33 -0500

    ObjectivesManager Added
    Basic quest tracker. Trying to get log (press l) to pop up in front of
    terrain. Same problem as HUDCanvas was having.

---
+++ Game/Assets/Scripts/ObjectivesManager.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ObjectivesManager : MonoBehaviour {
+    bool questLogOpened = false;
+    Canvas HUDcanvas;
+	// Use this for initialization
+	void Start () {
+        HUDcanvas = FindObjectOfType<Canvas>();
+	}
+	
+	// Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown("l"))
+        {
+            questLogOpened = !questLogOpened;
+            print("questLogOpened = " + questLogOpened);
+        }
+    }
+
+    void OnGUI()
+    {
+        if (questLogOpened)
+        {
+            
+            GUI.Box(new Rect(100, 100, Screen.height, Screen.width), "Test");
+        }
+    }
+}
