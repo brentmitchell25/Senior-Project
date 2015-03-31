8104e88988dd454d537b0b9731c00fe90119f849
brentmitchell25 <brent.mitchell25@gmail.com>
2015-03-31 14:37:38 -0500

    Added lighting to the scene

--- Game/Assets/Scripts/Turn Based Combat/SimpleEnemyAttack.cs
+++ Game/Assets/Scripts/Turn Based Combat/SimpleEnemyAttack.cs
@@ -55,6 +55,11 @@ public class SimpleEnemyAttack : MonoBehaviour {
         //Add time since Update was last called to timer
         timer += Time.deltaTime;
 
+		if (timer >= timeBetweenAttacks) {
+			Debug.Log(playerInRange);
+			Debug.Log(enemyHealth.curHealth > 0);
+			Debug.Log(!enemyMovement.AggroReset);
+				}
         //If timer exceeds time between attacks, you should attack player
         if (timer >= timeBetweenAttacks && playerInRange && enemyHealth.curHealth > 0 && !enemyMovement.AggroReset)
             Attack();
