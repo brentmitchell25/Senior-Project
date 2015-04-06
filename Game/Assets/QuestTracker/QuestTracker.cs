2de748e2dc587e9fbba4f385423a880d9a791055
brentmitchell25 <brent.mitchell25@gmail.com>
2015-04-05 21:42:37 -0500

    Got basic quest listing working for Forest scene. Press 'tab' or 'L' to view. Displays checkmark next to completed quests. Need to add quests and populate them dynamically.

---
+++ Game/Assets/QuestTracker/QuestTracker.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class QuestTracker : MonoBehaviour {
+	private CanvasGroup questTracker;
+	private Toggle quest1;
+	private Toggle quest2;
+	private Toggle quest3;
+	// Use this for initialization
+	void Start () {
+		questTracker = GameObject.Find ("QuestContainer").GetComponent<CanvasGroup>() as CanvasGroup;
+		quest1 = (Toggle)GameObject.Find ("Quest 1").GetComponent<Toggle>() as Toggle;
+		quest2 = (Toggle)GameObject.Find ("Quest 2").GetComponent<Toggle>() as Toggle;
+		quest3 = (Toggle)GameObject.Find ("Quest 3").GetComponent<Toggle>() as Toggle;
+
+		quest2.isOn = true;
+		setDisabled ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown ("tab") || Input.GetKeyDown ("l")) {
+			setEnabled();
+				}
+
+		if(Input.GetKeyUp ("tab") || Input.GetKeyUp ("l")) {
+			setDisabled();
+		}
+	}
+
+	void setEnabled() {
+		questTracker.alpha = 1;
+
+		}
+
+	void setDisabled() {
+		questTracker.alpha = 0;
+	}
+}
