a76708f5136dd97cd9113645bacb5265d2515c51
brentmitchell25 <brent.mitchell25@gmail.com>
2015-05-06 00:07:53 -0500

    Added sword to scene. Added new windows build (without sword)

---
+++ Game/Assets/Scripts/SetWeapon.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SetWeapon : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		Debug.Log (GameInformation.PlayerClass);
+		GameObject.Find ("Sword").GetComponent<Renderer> ().enabled = false;;
+
+		if(GameInformation.PlayerClass.ToString().Equals("BaseWarriorClass")) {
+			GameObject.Find ("Sword").GetComponent<Renderer>().enabled = true;
+
+		}
+
+		if(GameInformation.PlayerClass.ToString().Equals("BaseRangedClass")) {
+			GameObject.Find ("Sword").GetComponent<Renderer>().enabled = false;
+			// Stub
+		}
+
+		if(GameInformation.PlayerClass.ToString().Equals("BaseRangedClass")) {
+			GameObject.Find ("Sword").GetComponent<Renderer>().enabled = false;
+			// Stub
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
